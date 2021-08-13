8743c445069d77163dd6a8c734a29b0e77f2bc92
Jen Cruz <JENCRUZ@boisestate.edu>
2021-08-13 11:47:30 -0600

    started to clean data.

---
+++ ING_dataprep.R
@@ -0,0 +1,74 @@
+############################################################################
+####    This script was developed by Jen Cruz to clean and visualize     ###
+## black-tailed jackrabbit count data collected by Idaho National Guard ####
+## using single visit spotlight surveys conducted (near new moon) once a ###
+##  month from Apr-Sep of 2013 to 2020.                                  ###
+### Surveys include other species. In 2020 methods were improved to collect #
+# bearing and distance as well as details of nearby vegetation.           ##
+############################################################################
+##### Set up your workspace and load relevant packages -----------
+# Clean your workspace to reset your R environment. #
+rm( list = ls() )
+
+
+# load packages relevant to this script:
+library( tidyverse ) #easy data manipulation
+library( lubridate ) #easy date and time manipulation
+library( sf ) #for spatial data
+
+## end of package load ###############
+
+###################################################################
+#### Load or create data -----------------------------------------
+# Set working directory. This is the path to your Rstudio folder for this 
+# project. If you are in your correct Rstudio project then it should be:
+getwd()
+# if so then:
+workdir <- getwd()
+
+# set path to where you can access the Access database in your computer. #
+# Note that the path will be different in yours than mine.#
+datapath <- "Z:/Common/Jackrabbits/"
+#import relevant tables, which have been exported from Access database as #
+# .csv files# 
+
+#import records: calls file by pasting datapath to filename:
+rawdata <- read.csv( file = paste0( datapath,"BTJR_alldata.csv" ),
+                     #replaces those values with NA
+                     na.strings = c(""," ","NA"), 
+                     # includes column heading
+                     header = TRUE )
+
+#view
+head( rawdata ); dim(rawdata)
+
+#import track shapefile
+tracks <- sf::st_read( paste0( datapath, "2020_BTJRRoutes_ALL.shp" ) )
+#import observations as shapefile
+rawlocs <- sf::st_read( paste0( datapath, "JackrabbitData_All.shp" ) )
+#import NCA shapefile
+NCA <- sf::st_read( "Z:/Common/QCLData/Habitat/NCA/GIS_NCA_IDARNGpgsSampling/BOPNCA_Boundary.shp")
+
+#######################################################################
+######## cleaning data ###############################################
+
+# Check if shapefiles are in the same coordinate system
+tracks
+rawlocs
+NCA
+# yes. In NAD83 zone11N (so eastings and northings)
+#plot 
+ggplot( NCA ) +
+  theme_classic( base_size = 17 ) +
+  geom_sf() + 
+  geom_sf( data = tracks ) #+
+  #geom_sf( data = rawlocs )
+
+# rawlocs and rawdata may be the same 
+colnames( rawlocs )
+colnames( rawdata )
+# possibly. Start with rawlocs
+# remove some 
+rabbit_df <- rawlocs 
+
+####################### end of script #########################################
