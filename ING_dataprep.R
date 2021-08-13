deaff0a71e705962b659dfd7b26eddf0c2b80859
Jen Cruz <JENCRUZ@boisestate.edu>
2021-08-13 16:09:45 -0600

    Found mistakes with coordinates, in original clean up.

--- ING_dataprep.R
+++ ING_dataprep.R
@@ -13,6 +13,8 @@ rm( list = ls() )
 
 # load packages relevant to this script:
 library( tidyverse ) #easy data manipulation
+# set option to see all columns and more than 10 rows
+options( dplyr.width = Inf, dplyr.print_min = 100 )
 library( lubridate ) #easy date and time manipulation
 library( sf ) #for spatial data
 
@@ -57,18 +59,64 @@ tracks
 rawlocs
 NCA
 # yes. In NAD83 zone11N (so eastings and northings)
-#plot 
-ggplot( NCA ) +
-  theme_classic( base_size = 17 ) +
-  geom_sf() + 
-  geom_sf( data = tracks ) #+
-  #geom_sf( data = rawlocs )
 
 # rawlocs and rawdata may be the same 
 colnames( rawlocs )
 colnames( rawdata )
 # possibly. Start with rawlocs
-# remove some 
-rabbit_df <- rawlocs 
+# remove extra columns:
+all_df <- rawdata %>% 
+  dplyr::select( TranName, Species, NumInd, Dated, ObTime, TempF, Temperatur,
+    Habitat, dominantsh, nonshrub, isshrub, Distance, DistanceM, 
+    Side, Bearing, Comments, Comment, 
+    EASTING, NORTHING, ELEV) #, geometry )
+
+#check
+head( all_df ); dim( all_df )
+str(all_df)
+#combine similar columns
+all_df <- all_df %>% rowwise() %>% 
+  mutate( Temp = sum( TempF, Temperatur),
+          Dist = sum(Distance, DistanceM ),
+          Comments = paste( Comments, Comment, sep = " " ), 
+          Hab = paste( Habitat, dominantsh, nonshrub, sep = " " ))%>% 
+  dplyr::select( -TempF, -Temperatur, -Distance, -DistanceM,
+          -Comment, -Habitat, -dominantsh, -nonshrub, -isshrub )
+#check
+head( all_df ); dim( all_df )
+
+# NA are 1 I think
+all_df$NumInd[ is.na(all_df$NumInd) ] <- 1
+# Turn date into lubridate
+all_df <- all_df %>% 
+  mutate( Date = lubridate::dmy( Dated ),
+          Year = lubridate::year( Date ),
+          Month = lubridate::month( Date ) )
+
+#what species were detected:
+unique( all_df$Species )
+
+
+#select rabbit records only, with location data
+rabbit_df <- all_df %>% 
+  #keep only jackrabbit records
+  dplyr::filter( Species == "BTJR" ) %>% 
+  filter( !is.na(EASTING) ) %>% 
+  #turn into an sf dataframe using CRS from tracks
+  st_as_sf( .,coords = c( 'NORTHING', 'EASTING'),
+            crs = st_crs(tracks) )
+  
+#check
+head( rabbit_df); dim( rabbit_df )
+
+###########################################################
+#### visualize data
+#plot 
+ggplot( rabbit_df ) +
+  theme_classic( base_size = 17 ) +
+  geom_sf() + 
+   geom_sf( data = tracks ) +
+   facet_wrap( ~Year )
+ #geom_sf( data = NCA )
 
 ####################### end of script #########################################
