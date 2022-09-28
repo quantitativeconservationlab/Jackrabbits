8f44ab8f84f4110ba078da9422b54bd8626fb791
leticiacamacho <leticiacamacho@u.boisestate.edu>
2022-09-28 10:32:51 -0600

    Got route and BTJR obs- as points -with correct crs and added to map. Still cant figure out how to get line string to plot on map though - but can successfully get it to correct crs

--- BTJRDataPrep_300mGrids.R
+++ BTJRDataPrep_300mGrids.R
@@ -303,6 +303,7 @@ for( i in 1:dim(BTJRdf)[1] ) {
   BTJRdf$NewDate.Time[i]<-paste(a [1], b, sep = " ")
 }
 
+
 view(BTJRdf)
 #has new date.time column with format able to work with lubridate package
 
@@ -365,90 +366,6 @@ view(BTJRdf)
 
 
 
-
-# Manipulating Data: ------------------------------------------------------
-
-
-##Convert geometry from points to a line for routes:  -----------
-
-
-#southern route:  -----------
-
-#checking current geometry classification
-class(st_geometry(route_S)) 
-#"sfc_POINT" "sfc"
-
-
-#converting from point to line string:
-
-#Creating an object for the geometry column in the south route df
-Sgeo<-route_S$geometry
-#A step you need to do to inform the function below that is building the line
-n<-length(Sgeo)-1
-#creating function that is creating line strings from the point data 
-Sroute<-lapply(X=1:n, FUN=function(x){
-  pair<-st_combine(c(Sgeo[x], Sgeo[x+1]))
-  line<-st_cast(pair,"LINESTRING")
-  return(line)
-})
-
-
-#combining all the line strings together to create the continuous route 
-Sroute_line<-st_multilinestring(do.call("rbind",Sroute))
-
-#view new line
-plot(Sroute_line)
-class(Sroute_line)
-#[1] "XY"              "MULTILINESTRING" "sfg" 
-
-
-
-
-#northern route:  -----------
-
-#checking current geometry classification
-class(st_geometry(route_N)) 
-#"sfc_POINT" "sfc"
-
-#Creating an object for the geometry column in the south route df
-Ngeo<-route_N$geometry
-#A step you need to do to inform the function below that is building the line
-n2<-length(Ngeo)-1
-#creating function that is creating line strings from the point data 
-Nroute<-lapply(X=1:n2, FUN=function(x){
-  pair<-st_combine(c(Ngeo[x], Ngeo[x+1]))
-  line<-st_cast(pair,"LINESTRING")
-  return(line)
-})
-
-#combining all the line strings together to create the continuous route 
-Nroute_line <- st_multilinestring(do.call("rbind",Nroute))
-
-#view new line
-plot(Nroute_line)
-class(Nroute_line)
-#[1] "XY"              "MULTILINESTRING" "sfg" 
-
-
-
-
-
-
-#######################JC NOTES FROM MEETING: ########################
-Nroute_2 <- do.call( what = sf:::rbind.sf,
-                     args = Nroute )
-
-#### IT LOOKS LIKE THIS IS WHERE WE LOOSE THE CRS OF THE LINE 
-#### WHEN WE CHANGE FROM N_ROUTE TO NROUTE_LINE
-head( Nroute)
-
-
-### NEED TO FIX THIS HERE SO THAT THE CRS IS THE SAME AS IT WAS BEFORE CONVERTING
-# TO MULTILINE STRINGS  
-
-### NEED ASK MATT HOW TO CONVERT CRS OF  A MULTI LINE STRING TO WGS 84
-
-#################################################################
 
 
 
@@ -536,67 +453,18 @@ plot(st_geometry(NCAb_trans), add=TRUE)
 
 
 
+# Convert Route and BTJR Points to Raster CRS -----------------------------
 
-
-
-
-###########################################################################################################
-# change route_lines to same crs as NCA boundary / or raster 
-
-#Nrouteline_trans<- sf::st_transform( Nroute_line, st_crs( NCAgrid300m ) )#ERROR
-#i think it is giving an error because it is a multi line string 
-identicalCRS(as(Nroute_line, "Spatial"), NCAgrid300m)
-#FALSE
-new.crs<-CRS("+init=epsg:4326")#WGS84
-##########THIS WAY NOT SF PACKAGE COMPATIABLE THIS =SP PACKAGE ###############
-###########JC MEETING NOTES:
-st_crs( Nroute_line)
-class( Nroute_line)
-head( Nroute_line) 
-Nroute_pr <-  Nroute_line
-
-st_crs(Nroute_pr) = st_crs(4326) 
-
-Nroute.proj<- Nroute_line %>% st_transform(., new.crs)#didnt work - not creating object even 
-#Nroute.proj<-spTransform(Nroute_line, new.crs) 
-#also didnt work 
-
-#######################STOPPING HERE BECASUE CANT GET MULTILINE STRING OF ROUTES
-# TO MATCH THE RASTER YET HERE ############################
-############################ MOVING ON TO CLEANING RAB DF #############
-
-####################################################
-
-#Check if it worked:
-st_crs(Nroute.proj) == st_crs(NCAgrid300m)#FALSE
-
-############################################################################################
-## Try plotting BTJR points on map:
-plot(BTJRdf$geometry)
-crs(BTJRdf$geometry)#WGS84
-
-plot(NCAgrid300m)
-plot(st_geometry(BTJRdf), add=TRUE)#Can not see BTJR points on map 
-#Do we need to crop to be able to see points?????
-
-
-
-
-
-
-
-##################################### 27 Sept. 2022 - try to convert crs of routes to WGS 84 - same as raster
-
+##Northern Route:  -----------
 
 routeN_coord<-route_N # THIS WORKED !BUT WANT TO SEE IF WILL WORK WITH LINE TOO =======  IT DOES NOT
 #replacing route_N , with Nroute_line does not work ===== Error in UseMethod("st_as_sf") :  no applicable method for 'st_as_sf' applied to an object of class "c('XY', 'MULTILINESTRING', 'sfg')"
 
 #routeN_coord<-Nroute # also doesnt work : gives same error message:======Error in UseMethod("st_as_sf") : no applicable method for 'st_as_sf' applied to an object of class "list"
 
-
-
 route_coord_crs<- sp::CRS("+proj=longlat
                            +datum=WGS84")
+
 
 Ncoordinates<-sf:: st_as_sf(routeN_coord, 
                             coords=c("Longitude", "Latitude"),
@@ -604,14 +472,12 @@ Ncoordinates<-sf:: st_as_sf(routeN_coord,
 
 st_crs(Ncoordinates)
 
-
 Ncoordinates_aes<-sf::st_transform(Ncoordinates, crs(NCAgrid300m))
-
 
 st_crs(Ncoordinates_aes) == st_crs(NCAgrid300m)#TRUE!!
 #sf :: st_crs
 
-
+#View:
 plot(NCAgrid300m)
 plot(st_geometry(Ncoordinates_aes), add=TRUE)
 
@@ -619,25 +485,161 @@ plot(st_geometry(Ncoordinates_aes), add=TRUE)
 
 
 
-plot(st_geometry(BTJRdf), add=TRUE) #### DONT KNOW HOW TO ADD BTJR POINTS ON LINE YET 
+## Southern route:  ----------- 
+routeS_coord<-route_S
 
-################################################################################
+Scoordinates<-sf:: st_as_sf(routeS_coord, 
+                            coords=c("Longitude", "Latitude"),
+                            crs=route_coord_crs)
 
-#NOW TRY TO GET THE LINES (EITHER Nroute (POINTS) OR Nroute_line(LINES)) TO MATCH 
-#RASTER CRS - WGS 84 
-########################
+st_crs(Scoordinates)
 
+Scoordinates_aes<-sf::st_transform(Scoordinates, crs(NCAgrid300m))
 
+st_crs(Scoordinates_aes) == st_crs(NCAgrid300m)#TRUE!!
+#sf :: st_crs
 
-
-
-##############
-
-
-
-
+plot(NCAgrid300m)
+plot(st_geometry(Scoordinates_aes), add=TRUE)
 
 
+
+
+## BRJR points:  -----------
+
+BTJR_coord<-BTJRdf$geometry
+
+BTJRcoordinates<-sf:: st_as_sf(BTJR_coord, 
+                            coords=c("Longitude", "Latitude"),
+                            crs=route_coord_crs)
+
+st_crs(BTJRcoordinates)
+
+BTJRcoordinates_aes<-sf::st_transform(BTJRcoordinates, crs(NCAgrid300m))
+
+st_crs(BTJRcoordinates_aes) == st_crs(NCAgrid300m)#TRUE!!
+#sf :: st_crs
+
+#View:
+plot(NCAgrid300m)
+plot(st_geometry(BTJRcoordinates_aes), add=TRUE)#WORKED
+
+
+
+
+###############################################################################################
+
+# Changing Routes to Lines While Keeping CRS: -----------------------------
+
+## Checking current class of spatial object:  -----------
+class(Scoordinates_aes)#[1] "sf"         "data.frame"
+#BTJRcoordinates = also "sf" dataframe
+class(Scoordinates_aes$geometry)#[1] "sfc_POINT" "sfc" 
+
+## Changing Points to lines:
+S_coord_line<-sf:: st_cast(Scoordinates_aes$geometry, "LINESTRING")
+class(S_coord_line)#[1] "sfc_LINESTRING" "sfc"
+terra::crs(S_coord_line)#WGS84
+
+#View:
+plot(NCAgrid300m)
+plot(st_geometry(S_coord_line), add=TRUE)#DOES NOT WORK - Error in CPL_geos_is_empty(st_geometry(x)) : Evaluation error: IllegalArgumentException: point array must contain 0 or >1 elements.
+
+
+#####################################################################################################
+
+
+
+
+
+
+
+
+############################################################################
+## PAST WAY: CHANGING ROUTE POINTS TO LINES - BUT LOOSE CRS
+# Manipulating Data: ------------------------------------------------------
+
+
+##Convert geometry from points to a line for routes:  -----------
+
+
+#southern route:  -----------
+
+#checking current geometry classification
+class(st_geometry(route_S)) 
+#"sfc_POINT" "sfc"
+
+
+#converting from point to line string:
+
+#Creating an object for the geometry column in the south route df
+Sgeo<-route_S$geometry
+#A step you need to do to inform the function below that is building the line
+n<-length(Sgeo)-1
+#creating function that is creating line strings from the point data 
+Sroute<-lapply(X=1:n, FUN=function(x){
+  pair<-st_combine(c(Sgeo[x], Sgeo[x+1]))
+  line<-st_cast(pair,"LINESTRING")
+  return(line)
+})
+
+
+#combining all the line strings together to create the continuous route 
+Sroute_line<-st_multilinestring(do.call("rbind",Sroute))
+
+#view new line
+plot(Sroute_line)
+class(Sroute_line)
+#[1] "XY"              "MULTILINESTRING" "sfg" 
+
+
+
+
+#northern route:  -----------
+
+#checking current geometry classification
+class(st_geometry(route_N)) 
+#"sfc_POINT" "sfc"
+
+#Creating an object for the geometry column in the south route df
+Ngeo<-route_N$geometry
+#A step you need to do to inform the function below that is building the line
+n2<-length(Ngeo)-1
+#creating function that is creating line strings from the point data 
+Nroute<-lapply(X=1:n2, FUN=function(x){
+  pair<-st_combine(c(Ngeo[x], Ngeo[x+1]))
+  line<-st_cast(pair,"LINESTRING")
+  return(line)
+})
+
+#combining all the line strings together to create the continuous route 
+Nroute_line <- st_multilinestring(do.call("rbind",Nroute))
+
+#view new line
+plot(Nroute_line)
+class(Nroute_line)
+#[1] "XY"              "MULTILINESTRING" "sfg" 
+
+
+
+
+
+
+#######################JC NOTES FROM MEETING: ########################
+Nroute_2 <- do.call( what = sf:::rbind.sf,
+                     args = Nroute )
+
+#### IT LOOKS LIKE THIS IS WHERE WE LOOSE THE CRS OF THE LINE 
+#### WHEN WE CHANGE FROM N_ROUTE TO NROUTE_LINE
+head( Nroute)
+
+
+### NEED TO FIX THIS HERE SO THAT THE CRS IS THE SAME AS IT WAS BEFORE CONVERTING
+# TO MULTILINE STRINGS  
+
+### NEED ASK MATT HOW TO CONVERT CRS OF  A MULTI LINE STRING TO WGS 84
+
+#################################################################
 
 
 
