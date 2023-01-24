ccb7d18f22d9966d7e6d2593017ef783d02460fb
leticiacamacho <leticiacamacho@u.boisestate.edu>
2023-01-23 20:33:21 -0700

    Script developed to statistically analyze the BTJR summer 2022 spotlight survey data and determine optimal spotlight methodology

---
+++ SpotlightOpimization.R
@@ -0,0 +1,11 @@
+## This script was developed by Leticia Camacho to research the jackrabbit 
+##  spotlight survey 2022 data and determine optimal spotlight survey methodology
+
+# Setup -------------------------------------------------------------------
+
+## Set up your work space and load relevant packages -----------
+
+## Clean your work space to reset your R environment. 
+rm( list = ls() )
+
+
