6fb56d82418f789c4b3ecbd27e365bd7877fdc96
leticiacamacho <leticiacamacho@u.boisestate.edu>
2023-03-29 14:28:38 -0600

    template made and all save sf objects/data imported working on overlaying and calculating sampleing effort

--- SpotlightOpt_Raster2.R
+++ SpotlightOpt_Raster2.R
@@ -50,7 +50,8 @@ workdir <- getwd()
 
 
 # Load and Create Data ----------------------------------------------------
-# Set data paths:  -----------
+
+## Set data paths:  -----------
 
 #Creating pathway to call the habitat NLCD data from the common drive
 habpath <- "Z:/Common/QCLData/Habitat/" 
@@ -62,82 +63,59 @@ rastpath<-"Z:/Common/QCLData/Habitat/NLCD_new/NCA_raster_summaries_300m/"
 
 
 
-
-
 # Importing Data: ---------------------------------------------------------
 
 ## Import Rabbit Data:  -----------
 
-# August22:  -----------
-
-#Asite <- read.csv(paste0(datapath, "Spotlights/Aug22/ARoute.csv"))
-#site level info for August 2022 spotlight surveys 
-
-#Arab <- read.csv(paste0(datapath, "Spotlights/Aug22/Arab.csv"))
-#Rab locations for August 2022 spotlight surveys 
-
-Arabsite <- read.csv(paste0(datapath, "Spotlights/Aug22/AJrabRoutes.csv"))
+Arabsite <- read.csv(paste0(datapath, "/Spotlights/Aug22/AJrabRoutes.csv"))
 #combined site level info and rabbit locations for aug22 spotlight surveys 
 
 #check:
-str(Arab)
-unique(Arab$Rab.Obv)#only jackrbabits 
 str(Arabsite)
+#several NAs in several columns 
+#need to asses this
+
+unique(Arabsite$Rab.Obv)#only jackrbabits 
+
+## Aug22 spatial data: 
+Arabsite_rast <- st_read(paste0(datapath,
+                                "Spotlights/Spatial.Data/Arabsite_rast.shp"))
+#importing jrabbit sf objects created in SpotlightOpt_Raster.R
+#validated geometry and matching crs to NCAgrid300m raster in SpotlightOpt_Raster
 
 
 # June22:  -----------
 
-#Jsite<-read.csv(paste0(datapath, "Spotlights/June22/JRoutes.csv"))
-#site level info for June 2022 spotlight surveys 
-
-#Jrab <- read.csv(paste0(datapath, "Spotlights/June22/Jrab.csv"))
-#Rab locations for June 2022 spotlight surveys 
-
 Jrabsite <- read.csv(paste0(datapath, "Spotlights/June22/JJrabRoutes.csv"))
 #combined site level info and rabbit locations for aug22 spotlight surveys 
 #check:
-str(Jrab)
-unique(Jrab$Rab.Obv)#jackrabbit , unknown, and cottontails
-str(Jrabsite)#only jackrab. - dont use jrab
+str(Jrabsite)
+unique(Jrabsite$Rab.Obv)#jackrabbit only
+head(Jrabsite)
+#june location data of each observed jackrab was taken using tablets and the 
+#exact time of each point was not collected using these devices instead of 
+#gps units - switched to gps units in aug22, = can calc. hour, duration, etc. for aug22
+summary(Jrabsite)
 
 
-
-##Create sfobjects out of the june and aug rab data:  -----------
-
-#June22:  -----------
-#Manipulating June df to include a sf geometry object/col:  -----------
-#Convert a dataframe to a sf object:
-Jrab_sf <- st_as_sf( Jrabsite, coords = c("lon", "lat"), 
-                     crs = 4326 )#WGS84
-# now will have a geometry column 
-#check:
-Jrab_sf
-
-#Aug22:  -----------
-#Manipulating AUG df to include a sf geometry object/col:  -----------
-#Convert a dataframe to a sf object:
-Arab_sf <- st_as_sf(Arabsite, coords = c("lon", "lat"), 
-                    crs = 4326)#WGS84
-# now will have a geometry column 
-#Check:
-Arab_sf
-
-
-
+## June22 spatial data: 
+Jrabsite_rast <- st_read(paste0(datapath,
+                                "Spotlights/Spatial.Data/Jrabsite_rast.shp"))
+#importing jrabbit sf objects created in SpotlightOpt_Raster.R
+#validated geometry and matching crs to NCAgrid300m raster in SpotlightOpt_Raster
 
 
 # Importing Routes:  -----------
 
+## Routes Spatial Data:
+Nroute_rast <- st_read(paste0(datapath,
+                              "Spotlights/Spatial.Data/Nroute_rast.shp"))
 
-N.Route <-sf::st_read(paste0(datapath,
-                             "Shapefiles/Aug22_Spotlights_shp/N_RouteLine.shp"))
-plot(N.Route)
 
-S.Route <-sf::st_read(paste0(datapath,
-                             "Shapefiles/Aug22_Spotlights_shp/S_RouteLine.shp"))
-plot(S.Route)
-#looks good 
-
+Sroute_rast <- st_read(paste0(datapath, 
+                              "Spotlights/Spatial.Data/Sroute_rast.shp"))
+#BTJR 2022 spotlight survey routes sf objects created in SpotlightOpt_Raster.R
+#validated geometry and matching crs to NCAgrid300m raster in SpotlightOpt_Raster
 
 
 ## Importing Raster Data:  -----------
@@ -146,9 +124,12 @@ plot(S.Route)
 NCAgrid300m<- raster::raster( paste0(rastpath,
                                      "c20_agg.img")) 
 
+
 #import NCA shapefile from habitat folder:  -----------
-NCAboundary <- sf::st_read( paste0( habpath, 
-                                    "NCA/GIS_NCA_IDARNGpgsSampling/BOPNCA_Boundary.shp"))
+NCAb_rast <-sf::st_read(paste0(datapath,
+                               "Spotlights/Spatial.Data/NCAb_rast.shp"))
+#NCA Boundary line sf object created in SpotlightOpt_Raster.R
+#validated geometry and matching crs to NCAgrid300m raster in SpotlightOpt_Raster
 
 
 
@@ -156,10 +137,6 @@ NCAboundary <- sf::st_read( paste0( habpath,
 
 
 # Evaluating Raster Layer  ------------------------------------------------
-
-# figure out what the 300m NCA habitat raster map specs are so that we can 
-# create a mathing blank raster:
-
 
 ##Checking Extents :  -----------
 terra::ext(NCAgrid300m)
@@ -172,6 +149,7 @@ NCAgrid300m
 
 
 
+
 # Creating a Blank Raster to Match Original  ------------------------------
 rast_template <- rast(ext(NCAgrid300m),resolution = 300, 
                       crs = st_crs(NCAgrid300m)$wkt)
@@ -180,68 +158,6 @@ plot(NCAgrid300m)
 
 
 
-# Matching CRS's, geometries and Projection Specs  -------------------------------------
-##Checking if geometry of spatial objects are valid :  -----------
-#Aug22 :  -----------
-all(st_is_valid(Arab_sf))#TRUE
-all(is.na(st_dimension(Arab_sf)))#no missing geometry
-#FALSE : is there NA=false for all 
-#June22 :  -----------
-all(st_is_valid(Jrab_sf))#TRUE
-all(is.na(st_dimension(Arab_sf)))#no missing geometry
-#FALSE : is there NA=false for all 
-#NCA boundary :  -----------
-all(sf:: st_is_valid(NCAboundary))#TRUE
-all(is.na(st_dimension(NCAboundary)))#no missing geometry
-#FALSE: is there NA=false for all
-#Routes :  -----------
-all(sf:: st_is_valid(N.Route))#TRUE
-all(is.na(st_dimension(N.Route)))#no missing geometry
-#FALSE: is there NA=false for all
-all(sf:: st_is_valid(S.Route))#TRUE
-all(is.na(st_dimension(S.Route)))#no missing geometry
-#FALSE: is there NA=false for all
-
-
-#Checking CRS of all objects :  -----------
-sf::st_crs(Jrab_sf)#WGS84, EPSG:4326
-sf::st_crs(Arab_sf)#WGS84, EPSG:4326
-
-sf::st_crs(NCAboundary)#NAD83 / UTM zone 11N + NAVD88 height
-
-sf::st_crs(NCAgrid300m)#+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs 
-
-sf::st_crs(rast_template)#PROJCRS["unknown",BASEGEOGCRS["GCS_unknown", DATUM["World Geodetic System 1984",
-
-sf::st_crs(N.Route)#WGS84
-sf::st_crs(S.Route)#WGS84
-
-
-#Transforming crs to match raster :  -----------
-#create a version that matches coordinates of the predictor raster:
-
-NCAb_rast <- sf::st_transform( NCAboundary, st_crs( NCAgrid300m ) )#worked
-#NCA boundary crs now same as NCSgrid300m raster 
-#Check that this is correct:
-st_crs(NCAb_rast) == st_crs(NCAgrid300m)
-#TRUE - so this is good to go
-
-#Check to see that the rest of the objects are in the same crs as raster:
-Jrab_rast<-sf::st_transform( Jrab_sf, st_crs( NCAgrid300m ) )
-st_crs(Jrab_rast) == st_crs(NCAgrid300m)
-#TRUE 
-
-Arab_rast<-sf::st_transform( Arab_sf, st_crs( NCAgrid300m ) )
-st_crs(Arab_rast) == st_crs(NCAgrid300m)
-#TRUE
-
-Nroute_rast<-sf::st_transform( N.Route, st_crs( NCAgrid300m ) )
-st_crs(Nroute_rast) == st_crs(NCAgrid300m)
-#TRUE 
-
-Sroute_rast<-sf::st_transform( S.Route, st_crs( NCAgrid300m ) )
-st_crs(Sroute_rast) == st_crs(NCAgrid300m)
-#TRUE 
 
 
 crs(NCAgrid300m)#+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0+datum=WGS84 +units=m +no_defs
@@ -271,3 +187,19 @@ plot(st_geometry(Sroute_rast), add=TRUE, col="blue")
 
 
 # Analyzing Spatial Data --------------------------------------------------
+
+
+
+
+
+
+
+## Save work space:   -----------
+# saving all data to the path
+save.image("SpotlightOpt_Raster2.RData")
+
+
+
+
+# loading the workspace
+#load("SpotlightOpt_Raster2.RData")
